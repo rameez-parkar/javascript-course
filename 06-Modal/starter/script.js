57b058f4d2176f233734f248cdcb01e5b245682c
Rameez Parkar <rparkar@tavisca.com>
2021-05-02 13:43:23 +0530

    completed modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonShowModals = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const keyDown = function(event) {
+  if(event.key.toLowerCase() === 'escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+}
+
+buttonShowModals.forEach((button) => {
+  button.addEventListener('click', openModal);
+});
+
+buttonCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', keyDown);
